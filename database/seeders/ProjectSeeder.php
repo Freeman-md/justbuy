ff2d1cf15fb4b43c659dd1c38aed585540633b12
Freeman-md <freemanmadudili@gmail.com>
2021-09-22 01:35:11 +0100

    Updated seeders

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $projects = [['E-Commerce Application With Integrated Payment Gateway', 'Built an e-commerce application during Aptech Global Techwiz 2021', 'https://github.com/Freeman-md/justbuy', 'https://xclusivejustbuy.herokuapp.com', 1, 'TailwindCSS, Alpine, Laravel, Livewire'],
+    ['Single Page Application - Shopping Cart ', '', 'https://github.com/Freeman-md/coursework', 'https://mevn-cart.netlify.app', 0, 'Mongo DB, Express, Vue, Node'],
+    ['E - Commerce Single Page Application', '', 'https://github.com/Freeman-md/djackets', 'https://djackets-vue.netlify.app', 1, 'Django, Django REST Framework, Vue'],
+    ['HIOS Notepad and Todo Application Clone - SPA', '', 'https://github.com/Freeman-md/notepad', 'https://vue-ts-notepad.netlify.app', 1, 'Vue, TypeScript, Vuetify, Firebase'],
+    ['Madam Boutique Online Store ', 'Madam Boutique is a sophisticated complete e-commerce application using HTML, CSS & JS', 'https://github.com/Freeman-md/madam-boutique', 'https://js-commerce.netlify.app', 1, 'HTML, CSS, Bootstrap 4, JavaScript, Jquery'],
+    ['Todo Application using React JS', '', 'https://github.com/Freeman-md/react-todos', 'https://react-bootstrap5-todos.netlify.app', 0, 'React JS, Bootstrap 5']]
+
+        for ($i = 0; $i < count($projects); $i++) {
+            Project::create([
+                'title' => $projects[$i][0],
+                'description' => $projects[$i][1],
+                'github' => $projects[$i][2],
+                'live' => $projects[$i][3],
+                'star' => $projects[$i][4],
+                'stacks' => $projects[$i][5]
+            ]);
+        }
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,18 @@ class UserSeeder extends Seeder
     public function run()
     {
         // Seed users
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
+
+        // Create admin user
+        User::create([
+            'name' => "Administrator",
+            'username' => "Admin",
+            'email' => "admin@admin.com",
+            'contact_number' => '1234556789',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$/6kM98PhYuEhum.1KN3mdedxrm7aGo/iHIQNaznqRycvAeYQkD2Wa', // &i902lYG8os3
+            'remember_token' => Str::random(10),
+            'role' => 'Admin'
+        ]);
     }
 }
